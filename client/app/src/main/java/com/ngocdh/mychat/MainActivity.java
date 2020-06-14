caf6dc172535d403e3fea6225772b9c1a43f2a18
DinhHongNgoc <dinhhongngoc383@gmail.com>
2020-06-15 01:07:08 +0700

    [client] add socket io

--- client/app/src/main/java/com/ngocdh/mychat/MainActivity.java
+++ client/app/src/main/java/com/ngocdh/mychat/MainActivity.java
@@ -3,12 +3,60 @@ package com.ngocdh.mychat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
+
+import java.net.URISyntaxException;
 
 public class MainActivity extends AppCompatActivity {
+    private final String TAG = "ahihi - " + this.getClass().getSimpleName();
+
+    private Button btnConnect;
+    private Button btnDisconnect;
+    private Socket socket;
+    {
+        try {
+            socket = IO.socket("http://192.168.1.6:3000/");
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mapping();
+
+        btnConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                socket.connect();
+                Toast.makeText(MainActivity.this, "Connected", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnDisconnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (socket != null) {
+                    socket.disconnect();
+                    Toast.makeText(MainActivity.this, "Disconnected", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
+
+    private void mapping() {
+        btnConnect = findViewById(R.id.btnConnect);
+        btnDisconnect = findViewById(R.id.btnDisconnect);
     }
 }
